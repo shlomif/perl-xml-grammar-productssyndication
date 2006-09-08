1ce16a46f5828461c9b1af47cf243ca9b7c83d79
shlomif <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2006-09-08 17:37:28 +0000

    Implemented the is_valid() method in the file.

--- perl/XML-Grammar-ProductsSyndication/lib/XML/Grammar/ProductsSyndication.pm
+++ perl/XML-Grammar-ProductsSyndication/lib/XML/Grammar/ProductsSyndication.pm
@@ -3,6 +3,14 @@ package XML::Grammar::ProductsSyndication;
 use warnings;
 use strict;
 
+use XML::Grammar::ProductsSyndication::ConfigData;
+use XML::LibXML;
+
+use Moose;
+
+has '_filename' => (isa => 'Str', is => 'rw');
+has '_data_dir' => (isa => 'Str', is => 'rw');
+
 =head1 NAME
 
 XML::Grammar::ProductsSyndication - an XML Grammar for ProductsSyndication.
@@ -16,6 +24,8 @@ Version 0.01
 our $VERSION = '0.01';
 
 =head1 SYNOPSIS
+
+    use XML::Grammar::ProductsSyndication;
 
     my $syndicate = 
         XML::Grammar::ProductsSyndication->new(
@@ -37,6 +47,90 @@ our $VERSION = '0.01';
         );
 
 =head1 FUNCTIONS
+
+=head2 XML::Grammar::ProductsSyndication->new({ arg1 => "value"...})
+
+The constructor - accepts a single hash reference with the following keys:
+
+=over 4
+
+=item 'source'
+
+A reference to a hash that contains the information for the source XML for the
+file. Currently supported is a C<'file'> key that contains a path to the file.
+
+=item 'data_dir'
+
+Points to the data directory where the DTD files, the XSLT stylesheet, etc.
+are stored. Should not be generally over-ridden.
+
+=back
+
+=cut
+
+sub new
+{
+    my $class = shift;
+    my $self = {};
+    bless $self, $class;
+    $self->_init(@_);
+    return $self;
+}
+
+sub _init
+{
+    my ($self, $args) = @_;
+
+    my $source = $args->{'source'} or
+        die "did not specify the source";
+    
+    my $file = $source->{file};
+
+    $self->_filename($file);
+
+    my $data_dir = $args->{'data_dir'} ||
+        XML::Grammar::ProductsSyndication::ConfigData->config('extradata_install_path')->[0];
+
+    $self->_data_dir($data_dir);
+    return 0;
+}
+
+=head2 $processor->is_valid()
+
+Checks if the filename validates according to the DTD.
+
+=cut
+
+sub is_valid
+{
+    my $self = shift;
+
+    my $dtd = 
+        XML::LibXML::Dtd->new(
+            "Products Syndication Markup Language 0.1.1",
+            File::Spec->catfile(
+                $self->_data_dir(), 
+                "product-syndication.dtd"
+            ),
+        );
+
+    my $p = XML::LibXML->new();
+    $p->validation(0);
+
+    my $dom = $p->parse_file($self->_filename());
+    
+    return $dom->validate($dtd);
+}
+
+=begin unused
+
+=head2 $processor->meta();
+
+This is to settle the Pod::Coverage. meta() is probably inserted by Moose.
+
+=end unused
+
+=cut
 
 =head1 AUTHOR
 
