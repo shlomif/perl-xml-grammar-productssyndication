58341e876e209dce6546469e10464bd7c1712f79
shlomif <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2008-06-17 08:33:13 +0000

    Updated the Changes and bumped the version number.

--- perl/XML-Grammar-ProductsSyndication/lib/XML/Grammar/ProductsSyndication.pm
+++ perl/XML-Grammar-ProductsSyndication/lib/XML/Grammar/ProductsSyndication.pm
@@ -28,11 +28,11 @@ XML::Grammar::ProductsSyndication - an XML Grammar for ProductsSyndication.
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 =head1 SYNOPSIS
 
